248ba5f865655f6331307852f7495fc18df9fd96
Eric Yang <ey3@berkeley.edu>
2023-08-16 12:53:43 -0700

    changed package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
-import App from './src/App';
-import reportWebVitals from './src/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 
 
